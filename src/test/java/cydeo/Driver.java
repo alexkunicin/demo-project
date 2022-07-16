721d4bdc845e36946fcaf1100c3a4ee01a1d2e37
kunic <kunicinalex@gmail.com>
2022-07-16 10:31:57 -0400

    trigger practice

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -44,6 +44,7 @@ public class Driver {
                         e.printStackTrace();
                     }
                     break;
+//________________________________
 
                 case "remote-firefox":
 
